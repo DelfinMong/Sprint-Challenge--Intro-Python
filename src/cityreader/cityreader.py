6cb467794021cdcf56e2160fcabf7f0890e03537
unknown <delfdevs@gmail.com>
2020-07-20 03:33:09 -0400

    sprint-python

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,7 +1,16 @@
+import csv
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City:
+      def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
 
+      def __str__(self):
+        return f'{self.name}, {self.lat}, {self.lon}'
+      
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
@@ -21,7 +30,16 @@ def cityreader(cities=[]):
   # Ensure that the lat and lon valuse are all floats
   # For each city record, create a new City instance and add it to the 
   # `cities` list
-    
+  
+  with open ('src/cityreader/cities.csv') as csvFile:
+    csv_reader = csv.reader(csvFile)
+    next(csv_reader)
+    for row in csv_reader:
+        city = row[0]
+        lat = row[3]
+        lon = row[4]
+        city_list = City(city, float(lat), float(lon))
+        cities.append(city_list)
     return cities
 
 cityreader(cities)
